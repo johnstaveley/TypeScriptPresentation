da3b55badd5701d1eece9530c63db7fac5d525f4
John Staveley <john@staveley.org>
2017-01-09 15:47:31 +0000

    final version

--- TypeScriptPresentation/app.ts
+++ TypeScriptPresentation/app.ts
@@ -1,4 +1,7 @@
-﻿class Greeter {
+﻿/// <reference path="scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="scripts/typings/moment/moment.d.ts" />
+/// <reference path="scripts/typings/toastr/toastr.d.ts" />
+class Greeter {
     element: HTMLElement;
     span: HTMLElement;
     timerToken: number;
@@ -9,11 +12,21 @@
         this.span = document.createElement("span");
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        toastr.options.positionClass = "toast-bottom-full-width";
+        toastr.options.timeOut = 2000;
     }
 
     start() {
-        // here is an ES6 lambda
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+        this.timerToken = setInterval(() => {
+            this.span.innerHTML = new Date().toUTCString();
+            let secondsPassed: number = moment(new Date()).seconds();
+            let message: string = `${secondsPassed} seconds have passed`;
+            if (secondsPassed % 30 === 0) {
+                toastr.warning(message);
+            } else if (secondsPassed % 10 === 0) {
+                toastr.info(message);
+            }
+        }, 1000);
     }
 
     stop() {
@@ -29,17 +42,5 @@ window.onload = () => {
 };
 
 // TODO:
-// point out ES6 code in app.ts, e.g. lambdas
-// run code and explain what it does
-// show transcompiled code app.js
-// point out usage of .map.js file
-// note warnings if you make an error or badly type something
-// note intellisense in editor
-// go through build steps in csproj file props and targets. Note this won't work on the build server
-// add in definitely typed libraries for moment and toastr and add references to app.ts
-// add in libraries via NuGet for jQuery, moment and toastr
-// into Index.html add css for toastr and js for jquery, moment, and toastr
-// add in additional logic to app.ts
-// demonstrate strongly typing, design time checking and new ES6 features
 // see resulting ES5 code and working example in the page
 
